9b8b094df7468cdce238979fa90ec89ae9a99a0e
Impuls3iv <sammy.unterman@gmail.com>
2019-10-24 20:56:46 -0400

    starting BC

--- scraper.js
+++ scraper.js
@@ -2,21 +2,19 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const admin = require("firebase-admin");
 const date = new Date();
-
+// pageNum is used for traverse the product listings pages and needs to be at this level
 let pageNum = 1;
-let productArray = [];
-let vendor = [];
-let productTitle = [];
-let plantType = [];
-let thcRange = [];
-let cbdRange = [];
-let productType = [];
-let price = [];
-let totalNumberOfPages = 1;
 
 // called by getResults()
-const fetchData = async () => {
+const fetchDataForOCSfullProductListings = async () => {
   let url = "https://ocs.ca/collections/all-cannabis-products?&page=" + pageNum
+  const result = await axios.get(url);
+  return cheerio.load(result.data);
+};
+
+// called by getResults()
+const fetchDataForOCSbestSellers = async () => {
+  let url = "https://ocs.ca";
   const result = await axios.get(url);
   return cheerio.load(result.data);
 };
@@ -37,6 +35,7 @@ function getRandomInt(min, max) {
 
 // this function is called first
 const getResults = async () => {
+  let productArray = [];
   // Get a database reference to our blog
   let db = admin.database();
   // create db path reference
@@ -54,55 +53,105 @@ const getResults = async () => {
   let bestSellersCBDrange = [];
   let bestSellersPrice = [];
 
-  do {
-    const $ = await fetchData();
-    // only grab the best sellers data once.
-    if (pageNum == 2) {
-      let refToTopSellers = $('.slick-slide');
-      // get top seller list
-      refToTopSellers.each((index, element) => {
-        bestSellersVendor.push($('.product-tile__info .product-tile__vendor', element).text());
-        bestSellersTitle.push($(' .product-tile__info .product-tile__title', element).text());
-        bestSellersPlantType.push($('.product-tile__info .product-tile__plant-type p', element).text());
-        bestSellersTHCrange.push($('.product-tile__properties li:nth-child(2) p', element).text());
-        bestSellersCBDrange.push($('.product-tile__properties li:nth-child(3) p', element).text());
-        bestSellersPrice.push($('product-tile__price', element).text());
-      });
-      // once done iterating through the best sellers, prep to send to db
-      bestSellersArray.push ({
-        vendors: [...bestSellersVendor],
-        productTitle: [...bestSellersTitle],
-        plantType: [...bestSellersPlantType],
-        thcRange: [...bestSellersTHCrange],
-        cbdRange: [...bestSellersCBDrange],
-        price: [...bestSellersPrice],
-        date
-      });
+  ///////////////////////////////
+  //
+  // FULL LISTING FROM bccannabisstores BELOW
+  //
+  ///////////////////////////////
+
+  ///////////////////////////////
+  //
+  // FULL LISTING FROM bccannabisstores ABOVE
+  //
+  ///////////////////////////////
+
+  ///////////////////////////////
+  //
+  //  BEST SELLERS FROM OCS BELOW
+  //
+  ///////////////////////////////
+  let $ = await fetchDataForOCSbestSellers();
+  // grab vendor
+  $('.product-carousel__products article h4').each((index, element) => {
+    if (index === 0 || index % 3 === 0) {
+      bestSellersVendor.push($(element).text());
     }
+  });
+  // grab title
+  $('.product-carousel__products .product-tile__data h3').each((index, element) => {
+      bestSellersTitle.push($(element).text());
+  });
+  // grab plant type
+  $('.product-carousel__products .product-tile__properties li:nth-child(1) p').each((index, element) => {
+      bestSellersPlantType.push($(element).text());
+  });
+  // grab thc range
+  $('.product-carousel__products .product-tile__properties li:nth-child(2) p').each((index, element) => {
+    bestSellersTHCrange.push($(element).text());
+  });
+  //grab cbd range
+  $('.product-carousel__products .product-tile__properties li:nth-child(3) p').each((index, element) => {
+    bestSellersCBDrange.push($(element).text());
+  });
+  $('.product-carousel__products .product-tile__info .product-tile__price').each((index, element) => {
+    bestSellersPrice.push($(element).text());
+  });
+
+  bestSellersArray.push ({
+    vendors: [...bestSellersVendor],
+    productTitle: [...bestSellersTitle],
+    plantType: [...bestSellersPlantType],
+    thcRange: [...bestSellersTHCrange],
+    cbdRange: [...bestSellersCBDrange],
+    price: [...bestSellersPrice],
+    date
+  });
+  ////////////////////////////////////////
+  //
+  //  BEST SELLERS FROM OCS ABOVE
+  //
+  ////////////////////////////////////////
+
+  ////////////////////////////////////////
+  //
+  //  FULL PRODUCT LISTING FROM OCS BELOW
+  //
+  ////////////////////////////////////////
+  let vendor = [];
+  let productTitle = [];
+  let plantType = [];
+  let thcRange = [];
+  let cbdRange = [];
+  let price = [];
+  let totalNumberOfPages = 1;
+
+  do {
+    $ = await fetchDataForOCSfullProductListings();
     // first check how many total pages there are
     totalNumberOfPages = parseInt($('.pagination li:nth-last-child(2)').text());
 
     // use fetched data to grab elements (and their text) and push into arrays defined above
+    // get vendor
     $('.product-tile__vendor').each((index, element) => {
       vendor.push($(element).text());
     });
-
+    // grab product title
     $('.product-tile__title').each((index, element) => {
       productTitle.push($(element).text());
      });
-
+    // grab plant type
     $('.product-tile__plant-type').each((index, element) => {
       plantType.push($(element).text());
     });
-
+    // grab thc range
     $('.product-tile__properties  li:nth-child(2) p').each((index, element) => {
       thcRange.push($(element).text());
     });
-
+    // grab cbd range
     $('.product-tile__properties  li:nth-child(3) p').each((index, element) => {
       cbdRange.push($(element).text());
     });
-
+    // grab price
     $('.product-tile__price').each((index, element) => {
       price.push($(element).text());
     });
@@ -112,7 +161,7 @@ const getResults = async () => {
       pageNum ++;
     };
     console.log(pageNum)
-    //Convert to an array so that we can sort the results.
+    // Convert to an array so that we can sort the results.
     // we need the IF because when you click to go to the next page, it just appends the product list with new data
     // so that means that the last page will have all the data, only if we went thru the pages from 1 .. n, one at a time
     if (totalNumberOfPages == pageNum) {
@@ -128,11 +177,25 @@ const getResults = async () => {
     }
     await sleep(getRandomInt(5000, 20000));
   } while (totalNumberOfPages > pageNum);
+  ////////////////////////////////////////
+  //
+  //  FULL PRODUCT LISTING FROM OCS ABOVE
+  //
+  ////////////////////////////////////////
+
+  ////////////////////////////////////////
+  //
+  //  SEND OCS DATA SETS TO FIREBASE BELOW
+  //
+  ////////////////////////////////////////
   // send data to DB
   pageRef.set(productArray);
   pageRefBestSellers.set(bestSellersArray);
-  console.log('arr', bestSellersArray);
-
+  ////////////////////////////////////////
+  //
+  //  SEND OCS DATA SETS TO FIREBASE ABOVE
+  //
+  ////////////////////////////////////////
   return productArray;
 };
 
