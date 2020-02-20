ebbfe3e65e695d37ae9a56169569a0a945631480
Sam <sammy.unterman@gmail.com>
2020-02-20 12:46:33 -0500

    removed references to firebase
    I think this would be better served as a strict nodejs app rather than have firebase stuff strewn throughout

--- scraper.js
+++ scraper.js
@@ -1,37 +1,22 @@
 const fetch = require('node-fetch');
-const admin = require("firebase-admin");
-const date = new Date();
 let settings = { method: "Get" };
 
 // this function is called first => from app.js
 const getResults = () => {
-    // Get a database reference to our blog
-    // create db path reference
-    let db = admin.database();
-    // TODO add your firebase database reference
-    let dbReference = db.ref("YourDBreference");
-
-    let dateToString = date.toString();
-    // TODO Add your collections to this array to identify what collection you are scraping in the DB
-    let pathArray = ['someArbitraryShopifyCollectionDescription'];
 
     // TODO Add your URLs to this array to scrape the data in the CORRESPONDING pathArray Index
     let urlArray = ["https://www.someShopifyStore/collections/someShopifyCollection/product.json"];
 
     // capture stores using Shopify
     for (let i = 0; i < urlArray.length; i++) {
-        if (pathArray.length === urlArray.length) {
-            let jsonURL = urlArray[i];
-            let pathRef = dbReference;
-            let pathFromShopify = pathArray[i];
-            let pageRef = pathRef.child(pathFromShopify + "/" + dateToString);
-            fetch(jsonURL, settings)
-                .then(res => res.json())
-                .then((JSONproductList) => {
-                    pageRef.set(JSONproductList);
-                    return;
-                })
-        }
+        let jsonURL = urlArray[i];
+        fetch(jsonURL, settings)
+            .then(res => res.json())
+            .then((JSONproductList) => {
+                //TODO - do something with the output. You can set it to a database, or return it to a front end
+                console.log(JSONproductList)
+                return;
+            })
     }
 };
 
