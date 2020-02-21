738e8a998e424b37509b0ecd6d80048efdf878f3
Impuls3iv <sammy.unterman@gmail.com>
2020-02-20 20:42:03 -0500

    set up branch to be library

--- README.md
+++
@@ -1,9 +0,0 @@
-Welcome to the simplest Shopify scraper for NodeJS!
-
-Use this as a starter application to effectively scrape a Shopify website. You can run it from the console, or deploy it with a cronjob to scrape and save data to your DB around the clock!
-
-1. Find all the "TODO"s throughout the application to see what info you need to update
-2. To start the app run "npm i" then "node app.js"
-3. The app.js file has the NodeJS stuff, the scraper.js file has the Shopify scraping stuff
-4. I left the cron.yaml file in, so if you decide to deploy, pay attention to its endpoint "/schedule" as relates to the app.js file
-
--- scraper.js
+++ scraper.js
@@ -2,22 +2,14 @@ const fetch = require('node-fetch');
 let settings = { method: "Get" };
 
 // this function is called first => from app.js
-const getResults = () => {
-
-    // TODO Add your URLs to this array to scrape the data in the CORRESPONDING pathArray Index
-    let urlArray = ["https://www.someShopifyStore/collections/someShopifyCollection/product.json"];
-
+exports.getResults = (urlArray) => {
     // capture stores using Shopify
     for (let i = 0; i < urlArray.length; i++) {
         let jsonURL = urlArray[i];
         fetch(jsonURL, settings)
             .then(res => res.json())
             .then((JSONproductList) => {
-                //TODO - do something with the output. You can set it to a database, or return it to a front end
-                console.log(JSONproductList)
-                return;
+                return JSONproductList;
             })
     }
 };
-
-module.exports = getResults;
